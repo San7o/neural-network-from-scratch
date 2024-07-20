4c7a24f03d2a73a0dc73b22a7fb55f59a69ab0fd
San7o <santigio2003@gmail.com>
2024-07-20 12:10:21 +0200

    Acrivation rectified linear function

--- neural_network_from_scratch/__main__.py
+++ neural_network_from_scratch/__main__.py
@@ -1,29 +1,39 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data # dataset generator
 
-# Take an input, dot product it with the weights, add the biases
+class Activation_ReLU:
+    """
+    Activation Rectified Linear Unit
+    """
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         # We want to normalize the random weights
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
+    """ 
+    Take an input, dot product it with the weights, add the biases
+    """
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
 def main():
-    # Input Batch
-    X = [ [1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8] ]
 
-    np.random.seed(0)
+    # Overrides some numpy defaults to make them more consistant
+    # on different machines
+    nnfs.init()
+    X, y = spiral_data(100, 3)
 
-    layer1 = Layer_Dense(4, 5)
-    layer2 = Layer_Dense(5, 2)
+    layer1 = Layer_Dense(2, 5)
+    activation1 = Activation_ReLU()
 
     layer1.forward(X)
-    print(layer1.output)
-    layer2.forward(layer1.output)
-    print(layer2.output)
+    activation1.forward(layer1.output)
+
+    print(activation1.output)
 
 if __name__ == "__main__":
     main()
