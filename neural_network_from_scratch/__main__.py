a65cee4390b4b05d6789caabc0b2a2b056a56995
San7o <santigio2003@gmail.com>
2024-07-20 10:59:27 +0200

    Batches and layer dense

--- neural_network_from_scratch/__main__.py
+++ neural_network_from_scratch/__main__.py
@@ -1,16 +1,29 @@
 import numpy as np
 
+# Take an input, dot product it with the weights, add the biases
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        # We want to normalize the random weights
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
 def main():
-    inputs = [1, 2, 3, 2.5]
+    # Input Batch
+    X = [ [1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8] ]
 
-    weights = [ [0.2, 0.8, -0.5, 1.0],
-                [0.5, -0.91, 0.26, -0.5],
-                [-0.26, -0.27, 0.17, 0.87] ]
+    np.random.seed(0)
 
-    bias = [ 2, 3, 0.5 ]
+    layer1 = Layer_Dense(4, 5)
+    layer2 = Layer_Dense(5, 2)
 
-    output = np.dot(weights, inputs) + bias
-    print(output)
+    layer1.forward(X)
+    print(layer1.output)
+    layer2.forward(layer1.output)
+    print(layer2.output)
 
 if __name__ == "__main__":
     main()
