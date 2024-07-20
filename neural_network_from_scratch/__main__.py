5024b4bf48f53a2fd73c3a122951a1c24ef51677
San7o <santigio2003@gmail.com>
2024-07-20 13:06:43 +0200

    Activation softmax

--- neural_network_from_scratch/__main__.py
+++ neural_network_from_scratch/__main__.py
@@ -5,11 +5,31 @@ from nnfs.datasets import spiral_data # dataset generator
 class Activation_ReLU:
     """
     Activation Rectified Linear Unit
+
+    If the input is less than 0, we set it to 0. Otherwise we keep the value
+    We need this to introduce non-linearity in the network
     """
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
+class Activation_Softmax:
+    """
+    Activation Softmax
+
+    The softmax function is used to normalize the output of a layer as probabilities
+    """
+    def forward(self, inputs):
+        # We sybtract the max value to prevent overflow. The result will
+        # be from 0 to 1
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # Normalize the values
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
 class Layer_Dense:
+    """
+    A layer of the network
+    """
     def __init__(self, n_inputs, n_neurons):
         # We want to normalize the random weights
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -25,15 +45,22 @@ def main():
     # Overrides some numpy defaults to make them more consistant
     # on different machines
     nnfs.init()
-    X, y = spiral_data(100, 3)
+    X, y = spiral_data(samples=100, classes=3)
 
-    layer1 = Layer_Dense(2, 5)
+    dense1 = Layer_Dense(2, 3)
     activation1 = Activation_ReLU()
 
-    layer1.forward(X)
-    activation1.forward(layer1.output)
+    # Output layer
+    dense2 = Layer_Dense(3, 3)
+    activation2 = Activation_Softmax()
 
-    print(activation1.output)
+    dense1.forward(X)
+    activation1.forward(dense1.output)
+
+    dense2.forward(activation1.output)
+    activation2.forward(dense2.output)
+
+    print(activation2.output[:5])
 
 if __name__ == "__main__":
     main()
